90723bc2dfd0ccec60df1d8a75ae8a539356587d
George Elkik <gelkik@gmail.com>
2023-02-28 22:14:24 -0500

    final

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,18 @@
 # your code goes here!
+import time
+def countdown(x):
+    while x >= 0:
+        if x != 0: 
+            print(f"{x} SECOND(S)!")
+        else:
+            print('HAPPY NEW YEAR!')
+        x-=1
+
+def countdown_with_sleep(x):
+    while x >= 0:
+        if x != 0: 
+            print(f"{x} SECOND(S)!")
+            time.sleep(1)
+        else:
+            print('HAPPY NEW YEAR!')
+        x-=1
